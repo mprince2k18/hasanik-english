d6080469de3f8ae31a11bf91a250c388c0cbf706
mprince2k18 <mprince2k16@gmail.com>
2020-07-26 04:00:59 +0600

    course

--- resources/views/backend/app.blade.php
+++ resources/views/backend/app.blade.php
@@ -7,8 +7,8 @@
 
         <meta content="width=device-width, initial-scale=1" name="viewport"/>
         <meta charset="UTF-8">
-        <meta name="description" content="Admin Dashboard Template" />
-        <meta name="keywords" content="admin,dashboard" />
+        <meta name="description" content="hasanik english" />
+        <meta name="keywords" content="hasanik english,hasanikway learning" />
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="author" content="stacks" />
 
