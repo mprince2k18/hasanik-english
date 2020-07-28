2b9715e250f8803de686c69a864f5de7c03bf4e7
mprince2k18 <mprince2k16@gmail.com>
2020-07-28 14:54:50 +0600

    nav solved

--- resources/js/components/SingleBlogComponent.vue
+++ resources/js/components/SingleBlogComponent.vue
@@ -18,7 +18,7 @@
       <section class="blog-page">
         <div class="container">
           <div class="row">
-            <div class="col-lg-8">
+            <div class="col-lg-12">
               <div class="blog-posts">
                 <div class="row">
                   <div class="col-lg-12">
@@ -60,72 +60,7 @@
                 </div>
               </div>
             </div>
-            <div class="col-lg-4">
-              <div class="blog-sidebar">
-                <div class="row">
-                  <div class="col-lg-12">
-                    <div class="blog-widget about-me">
-                      <div class="about-thumb">
-                        <img src="images/about-me.jpg" alt="">
-                      </div>
-                      <h4>{{ blog.user.name }}</h4>
-                      <p>{{ blog.user.position }}</p>
-                      <ul class="social-icons">
-                        <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                        <li><a href="#"><i class="fa fa-behance"></i></a></li>
-                        <li><a href="#"><i class="fa fa-dribbble"></i></a></li>
-                      </ul>
-                    </div>
-                  </div>
-                  <div class="col-lg-12">
-                    <div class="blog-widget search">
-                      <form>
-                          <input type="search" value="" placeholder="Type to search..." required="">
-                          <button type="submit" class="primary-button"><i class="fa fa-search"></i></button>
-                      </form>
-                    </div>
-                  </div>
-                  <div class="col-lg-12">
-                    <div class="blog-widget categories">
-                      <div class="blog-heading">
-                        <h4>Categories</h4>
-                      </div>
-                      <ul>
-                        <li><router-link to="blog">Web Analysis<span>(2)</span></router-link></li>
-                        <li><router-link to="blog">Digital Marketing<span>(5)</span></router-link></li>
-                        <li><router-link to="blog">Creative Agencies<span>(4)</span></router-link></li>
-                        <li><router-link to="blog">Branding Design<span>(8)</span></router-link></li>
-                        <li><router-link to="blog">Graphic Design<span>(9)</span></router-link></li>
-                        <li><router-link to="blog">Web Re-Design<span>(11)</span></router-link></li>
-                      </ul>
-                    </div>
-                  </div>
-                  <div class="col-lg-12">
-                    <div class="blog-widget recent-posts">
-                      <div class="blog-heading">
-                        <h4>Recent Posts</h4>
-                      </div>
-                      <ul>
-                        <li v-for="recentBlog in recentBlogs.data" :key="recentBlog.id">
-                          <router-link :to="`/page/blog/${recentBlog.id}`">
-                            <div class="blog-thumb">
-                              <img :src="recentBlog.thumbnail" :alt="recentBlog.title">
-                            </div>
-                            <div class="right-content">
-                              <h6>{{ recentBlog.title }}</h6>
-                              <span><timeago :datetime="recentBlog.created_at" :auto-update="15"></timeago></span>
-                            </div>
-                          </router-link>
-                        </li>
-                        
-                      </ul>
-                    </div>
-                  </div>
-                  
-                </div>
-              </div>
-            </div>
+            
           </div>
         </div>
       </section>
