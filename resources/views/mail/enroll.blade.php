3a63e665ebb1dc8824eac626778db6e75b11dbb9
mprince2k18 <mprince2k16@gmail.com>
2020-07-29 23:22:29 +0600

    mail

--- resources/views/mail/enroll.blade.php
+++ resources/views/mail/enroll.blade.php
@@ -1,3 +1,3 @@
-Mr/Mrs. {{ $name }}, Your enrollment is successfully done. Our admission will contact with you within few hours. <br>
+Mr/Mrs. {{ $name }}, Your enrollment is successfully done. Our admission team will contact with you within few hours. <br>
 Thank you <br>
 Hasanik Learning
