2f4e146805cc6fcea10f78a204fbf4f7a8f6c059
mprince2k18 <mprince2k16@gmail.com>
2020-07-30 23:59:26 +0600

    Dashboard update

--- resources/views/dashboard/auth/login.blade.php
+++ resources/views/dashboard/auth/login.blade.php
@@ -76,7 +76,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>iofrm</title>
+    <title>{{ env('APP_NAME') }}</title>
     <link rel="stylesheet" type="text/css" href="{{ asset('auth/css/bootstrap.min.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('auth/css/fontawesome-all.min.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('auth/css/iofrm-style.css') }}">
