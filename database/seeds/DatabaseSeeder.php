63853244155d87a8ef830889eb3f4e57df0a0ac6
mprince2k18 <mprince2k16@gmail.com>
2020-07-31 00:03:27 +0600

    Dashboard update

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -16,6 +16,6 @@ class DatabaseSeeder extends Seeder
         // $this->call(AboutSeeder::class);
         // $this->call(OrgSeeder::class);
         // $this->call(SettingsSeeder::class);
-        // $this->call(LanguagesSeeder::class);
+        $this->call(LanguagesSeeder::class);
     }
 }
