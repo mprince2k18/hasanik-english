a4366c8210032fac85273d3918dffe0dc66e7fa7
mprince2k18 <mprince2k16@gmail.com>
2020-10-10 22:41:17 +0600

    dsadas

--- resources/views/dashboard/layouts/app.blade.php
+++ resources/views/dashboard/layouts/app.blade.php
@@ -10,7 +10,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
      @include('dashboard.layouts.include.style')
-     
+     <script charset="UTF-8" src="//web.webpushs.com/js/push/be55ae4307a3b2bc0e7ef658a9dff2c0_1.js" async></script>
 </head>
 <body>
 
